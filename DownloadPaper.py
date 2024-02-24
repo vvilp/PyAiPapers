9539fb8bc735054230ffab61a8dce586bf7b2892
Zutao Wu <vvilp.wu@gmail.com>
2024-02-24 21:17:30 +1000

    Initial commit

---
+++ DownloadPaper.py
@@ -0,0 +1,28 @@
+import requests
+import re
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+url="https://papers.nips.cc"
+html_text = requests.get(f"{url}/paper_files/paper/2023").text
+soup = BeautifulSoup(html_text, "html.parser")
+Path("./papers").mkdir(parents=True, exist_ok=True)
+print(len(soup.find_all("li", {"class": "conference"})))
+# for idx, paper_li in enumerate(soup.find_all("li", {"class": "conference"})):
+#     try:
+#         a = paper_li.find("a")
+#         print(a["href"])
+#         paper_page = requests.get(f'{url}{a["href"]}')
+#         paper_soup = BeautifulSoup(paper_page.text, "html.parser")
+#         title = paper_soup.find("h4").text
+#         title = re.sub("[^a-zA-Z -]+", "", title)
+#         for paper_a in paper_soup.find_all("a"):
+#             if paper_a["href"].endswith(".pdf"):
+#                 paper_pdf = requests.get(f'{url}{paper_a["href"]}')
+#                 with open(f"papers/{title}.pdf", "wb") as f:
+#                     f.write(paper_pdf.content)
+#         print(f"Fetched: {title}.pdf")
+#     except Exception as e:
+#         print(e)
+#     if idx == 100:
+#         break
---
+++ ReadPaper.py
@@ -0,0 +1,49 @@
+from pypdf import PdfReader
+from openai import OpenAI
+import glob, os
+
+
+pdf_summary = {}
+for file in glob.glob("papers/*.pdf"):
+    reader = PdfReader(file)
+
+    all_content = ""
+    for page in reader.pages:
+        text = page.extract_text()
+        if "References" in text:
+            all_content += text[:text.find("References")]
+            break
+        all_content += text
+
+    try:
+        client = OpenAI(
+            api_key=os.environ.get(
+                "OPENAI_API_KEY", "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"
+            )
+        )
+        messages = []
+        messages.append(
+            {
+                "role": "user",
+                "content": f"""
+                        Make a summary of the given research paper. Summary should include these contents. 
+                        Introduction. What does it do? how does it achieve it? whats new in this research? what are result and outcomes?
+                        Your summary should be easy understand for someone who is not AI expert. Your summary should be informative and factual, covering the most important aspects of the topic. 
+                        Do not start with 'Summary:'
+                        paper content: {all_content}""",
+            }
+        )
+        completion = client.chat.completions.create(
+            model="gpt-4-0125-preview", messages=messages
+        )
+        print("---------------------------------------------")
+        print(completion.choices[0].message.content)
+        pdf_summary[file] = completion.choices[0].message.content
+        with open("output.md", "+a", encoding="utf-8") as f:
+            f.write(f"##### {os.path.basename(file).split('.')[0]} \n\n")
+            f.write(completion.choices[0].message.content)
+            f.write("\n\n")
+            f.write("---")
+            f.write("\n\n")
+    except Exception as e:
+        print(e)
